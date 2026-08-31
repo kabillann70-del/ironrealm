5bad64c01d47e087dc9cd8e19afd8b0616924c11
kabillann70-del <kabillann70@gmail.com>
2026-08-31 02:56:21 +0000

    Updated item icons with custom images

--- items.js
+++ items.js
@@ -1,19 +1,41 @@
 // items.js
 const ITEMS = {
     raw_wood: { 
-        name: 'Raw Wood', type: 'material', 
-        image: 'https://kenney.nl/content/assets/rpg-base/preview.png', // Placeholder URL
-        sellValue: 5, price: 10 
+        name: 'Raw Wood', 
+        type: 'material', 
+        image: 'https://opengameart.org/sites/default/files/wood%20log%20sprite%20sheet_0.png',
+        sellValue: 5, 
+        price: 10 
     },
     raw_ore: { 
-        name: 'Iron Ore', type: 'material', 
-        image: 'https://kenney.nl/content/assets/rpg-base/preview.png',
-        sellValue: 8, price: 15 
+        name: 'Iron Ore', 
+        type: 'material', 
+        image: 'https://img.icons8.com/color/96/iron-ore.png', // Sketchfab links don't work as images, using this high-quality one instead
+        sellValue: 8, 
+        price: 15 
+    },
+    ogre_bone: { 
+        name: 'Ogre Bone', 
+        type: 'material', 
+        image: 'https://img.freepik.com/premium-vector/pixel-art-bone-icon_735839-2108.jpg',
+        sellValue: 15, 
+        price: 30 
     },
     wood_sword: { 
-        name: 'Wood Sword', type: 'weapon', dmg: 5, 
-        image: 'https://kenney.nl/content/assets/rpg-base/preview.png',
-        sellValue: 20, price: 50 
+        name: 'Wood Sword', 
+        type: 'weapon', 
+        dmg: 5, 
+        image: 'https://e1.pngegg.com/pngimages/228/288/png-clipart-minecraft-icon-1-4-wooden-sword-brown-sword-art.png',
+        sellValue: 20, 
+        price: 50 
+    },
+    novice_axe: {
+        name: 'Novice Axe',
+        type: 'tool',
+        toolType: 'tree',
+        image: 'https://opengameart.org/sites/default/files/item_stoneaxe.png',
+        sellValue: 15,
+        price: 40
     }
 };
 
